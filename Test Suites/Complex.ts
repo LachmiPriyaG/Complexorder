879ec9c5488174a3df9722d5d5f4d98900116194
LachmiPriyaG <62049035+LachmiPriyaG@users.noreply.github.com>
2020-03-12 15:01:03 +0530

    Add files via upload

---
+++ Test Suites/Complex.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Complex</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>882ab3c2-40c2-4b01-a468-b97b938f0a73</testSuiteGuid>
+   <testCaseLink>
+      <guid>097858dc-5d97-45b7-83a8-252dfbde2b12</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Complex</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
